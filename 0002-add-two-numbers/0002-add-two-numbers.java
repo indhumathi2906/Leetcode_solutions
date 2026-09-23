877d03db9794f5f8d6a8954ca98d33173801b2f4
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:02 +0530

    0002-add-two-numbers: handle edge cases for null input lists

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -13,6 +13,9 @@ class ListNode {
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
         ListNode dummyHead = new ListNode(0);
         ListNode curr = dummyHead;
         int carry = 0;
