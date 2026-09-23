54a7e0bb17376ab34bd2067024ea29f3125fe7d2
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:16 +0530

    Day 1 - Two Sum: add problem description Javadoc

--- Day1_TwoSum/Day1_TwoSum.java
+++ Day1_TwoSum/Day1_TwoSum.java
@@ -1,4 +1,8 @@
 import java.util.*;
 
+/**
+ * Day 1: 1. Two Sum (LeetCode #1)
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ */
 public class Day1_TwoSum {
 }
