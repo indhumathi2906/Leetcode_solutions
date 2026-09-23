61d27d66ea6a9f1b71ecba052a6b39a8dc4df391
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:54:59 +0530

    0001-two-sum: add input validation for null or small array

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,5 +6,9 @@ import java.util.*;
  */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[0];
+        }
+        return new int[0];
     }
 }
