fe65e1c9589026499e82028a8fd566c03449c59f
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:27 +0530

    Day 4 - Valid Parentheses: implement stack matching logic loop

--- Day4_ValidParentheses/Day4_ValidParentheses.java
+++ Day4_ValidParentheses/Day4_ValidParentheses.java
@@ -9,6 +9,12 @@ public class Day4_ValidParentheses {
             return false;
         }
         Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
         return stack.isEmpty();
     }
 }
