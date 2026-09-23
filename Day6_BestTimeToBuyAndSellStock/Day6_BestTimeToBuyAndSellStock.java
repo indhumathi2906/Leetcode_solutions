8fb16ae95db75be449740d61876be2e03432a59b
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:33 +0530

    Day 6 - Best Time to Buy and Sell Stock: implement single-pass greedy traversal

--- Day6_BestTimeToBuyAndSellStock/Day6_BestTimeToBuyAndSellStock.java
+++ Day6_BestTimeToBuyAndSellStock/Day6_BestTimeToBuyAndSellStock.java
@@ -8,6 +8,13 @@ public class Day6_BestTimeToBuyAndSellStock {
         }
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
         return maxProfit;
     }
 }
