1918996f21ca350587618ad3f3aabe456ff4e83b
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:15 +0530

    0121-best-time-to-buy-and-sell-stock: add complexity annotations

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,7 +1,8 @@
 /**
  * 121. Best Time to Buy and Sell Stock
- * You are given an array prices where prices[i] is the price of a given stock on the ith day.
- * Return the maximum profit you can achieve from this transaction.
+ *
+ * Time Complexity: O(N) single pass over price list.
+ * Space Complexity: O(1) constant auxiliary space.
  */
 class Solution {
     public int maxProfit(int[] prices) {
@@ -10,6 +11,7 @@ class Solution {
         }
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
+        // Single pass update
         for (int price : prices) {
             if (price < minPrice) {
                 minPrice = price;
