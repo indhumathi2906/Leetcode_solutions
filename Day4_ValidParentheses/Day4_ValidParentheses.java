610c6d3c4dbfe4f5cdba5cb7d87358e4d364ff4b
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:25 +0530

    Day 4 - Valid Parentheses: declare isValid method signature

--- Day4_ValidParentheses/Day4_ValidParentheses.java
+++ Day4_ValidParentheses/Day4_ValidParentheses.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 /**
  * Day 4: 20. Valid Parentheses (LeetCode #20)
- * Determine if input string containing brackets is valid.
  */
 public class Day4_ValidParentheses {
+    public boolean isValid(String s) {
+        return false;
+    }
 }
