fd7865bb12b5258ae25f474d5a1c53a1d7331ace
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:34 +0530

    Day 7 - Reverse Linked List: add problem description Javadoc

--- Day7_ReverseLinkedList/Day7_ReverseLinkedList.java
+++ Day7_ReverseLinkedList/Day7_ReverseLinkedList.java
@@ -6,5 +6,9 @@ class ListNode {
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
+/**
+ * Day 7: 206. Reverse Linked List (LeetCode #206)
+ * Given the head of a singly linked list, reverse the list and return reversed head.
+ */
 public class Day7_ReverseLinkedList {
 }
