9626adb6eb656af1580d296832ecef05cd4913e1
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:13 +0530

    0121-best-time-to-buy-and-sell-stock: add problem description Javadoc

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,2 +1,7 @@
+/**
+ * 121. Best Time to Buy and Sell Stock
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * Return the maximum profit you can achieve from this transaction.
+ */
 class Solution {
 }
