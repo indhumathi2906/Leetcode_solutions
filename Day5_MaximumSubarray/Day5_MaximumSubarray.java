2c5e4ec47048f14ab052da0454d0052883c7b93e
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:29 +0530

    Day 5 - Maximum Subarray: declare maxSubArray method signature

--- Day5_MaximumSubarray/Day5_MaximumSubarray.java
+++ Day5_MaximumSubarray/Day5_MaximumSubarray.java
@@ -1,6 +1,8 @@
 /**
  * Day 5: 53. Maximum Subarray (LeetCode #53)
- * Find subarray with largest sum using Kadane's Algorithm.
  */
 public class Day5_MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
 }
