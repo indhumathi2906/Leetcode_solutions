4ba882721298b0e338fcae20e1357a861537cb64
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:54:58 +0530

    0001-two-sum: declare twoSum method signature

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,4 +5,6 @@ import java.util.*;
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  */
 class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    }
 }
