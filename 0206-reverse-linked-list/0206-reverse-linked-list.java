4add64e1fecceb667ee5dfe95387fb0cf436c2f8
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:17 +0530

    0206-reverse-linked-list: implement iterative pointer reversal loop

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -17,6 +17,12 @@ class Solution {
         }
         ListNode prev = null;
         ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
         return prev;
     }
 }
