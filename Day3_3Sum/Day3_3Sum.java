b1e6648c5b80428e9de647a60e5792f5d544e7bc
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:22 +0530

    Day 3 - 3Sum: declare threeSum method signature

--- Day3_3Sum/Day3_3Sum.java
+++ Day3_3Sum/Day3_3Sum.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 /**
  * Day 3: 15. 3Sum (LeetCode #15)
- * Given an integer array nums, return all unique triplets that sum to zero.
  */
 public class Day3_3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        return new ArrayList<>();
+    }
 }
