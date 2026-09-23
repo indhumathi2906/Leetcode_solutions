4c78cf2bc9047228d1454600a3f58b54ed061373
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:11 +0530

    0053-maximum-subarray: implement Kadane's algorithm iteration

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -9,6 +9,10 @@ class Solution {
         }
         int maxSoFar = nums[0];
         int currentSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currentSum);
+        }
         return maxSoFar;
     }
 }
