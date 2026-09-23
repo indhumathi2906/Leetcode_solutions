0e8b936a5b3cb24955a03587250a5b3f367e2107
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:18 +0530

    Day 1 - Two Sum: polish code formatting and finalize solution

--- Day1_TwoSum/Day1_TwoSum.java
+++ Day1_TwoSum/Day1_TwoSum.java
@@ -1,19 +1,21 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Day 1: 1. Two Sum (LeetCode #1)
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * Link: https://leetcode.com/problems/two-sum/
  *
- * Time Complexity: O(N) where N is array length.
- * Space Complexity: O(N) for HashMap storage.
+ * Given an array of integers nums and an integer target, return indices of the 
+ * two numbers such that they add up to target.
+ *
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
  */
 public class Day1_TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        // Validate input array
         if (nums == null || nums.length < 2) {
             return new int[0];
         }
-        // Map value -> index
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
