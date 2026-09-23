2eee4cabdccddde6d84dd48e6ac3a9afdf7557cd
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:32 +0530

    Day 6 - Best Time to Buy and Sell Stock: declare maxProfit method signature

--- Day6_BestTimeToBuyAndSellStock/Day6_BestTimeToBuyAndSellStock.java
+++ Day6_BestTimeToBuyAndSellStock/Day6_BestTimeToBuyAndSellStock.java
@@ -1,6 +1,8 @@
 /**
  * Day 6: 121. Best Time to Buy and Sell Stock (LeetCode #121)
- * Single transaction stock profit maximization.
  */
 public class Day6_BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
 }
