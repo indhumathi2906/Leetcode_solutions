a2032171cda065c3bd656b12aed5ed3541e29710
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:10 +0530

    0053-maximum-subarray: declare maxSubArray method signature

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,4 +3,7 @@
  * Given an integer array nums, find the subarray with the largest sum, and return its sum.
  */
 class Solution {
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
 }
