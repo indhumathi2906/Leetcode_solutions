b8e4098e8f88b56b2fea5ba3df08799d35fd9487
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 09:07:29 +0530

    Day 5 - Maximum Subarray: add null/empty check

--- Day5_MaximumSubarray/Day5_MaximumSubarray.java
+++ Day5_MaximumSubarray/Day5_MaximumSubarray.java
@@ -3,6 +3,9 @@
  */
 public class Day5_MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        return nums[0];
     }
 }
