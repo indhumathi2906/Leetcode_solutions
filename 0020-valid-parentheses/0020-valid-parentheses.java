6b7b3114d38fbd0435423767e190d734e48b205e
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:06 +0530

    0020-valid-parentheses: add problem description Javadoc

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,4 +1,9 @@
 import java.util.*;
 
+/**
+ * 20. Valid Parentheses
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ */
 class Solution {
 }
