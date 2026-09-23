74d97aa2cc7e0c5f5459f10b2c243b3f9742668b
Indhumathi <indhumathi.a29@gmail.com>
2026-09-23 08:55:14 +0530

    0121-best-time-to-buy-and-sell-stock: implement single-pass greedy traversal

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -10,6 +10,13 @@ class Solution {
         }
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
         return maxProfit;
     }
 }
