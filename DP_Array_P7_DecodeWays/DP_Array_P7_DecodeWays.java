dba439d78b70bdaa9e4755ff1d26c1050b286d33
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:26 +0530

    DP Array P7 - Decode Ways: Implement 1D Bottom-Up DP table

--- DP_Array_P7_DecodeWays/DP_Array_P7_DecodeWays.java
+++ DP_Array_P7_DecodeWays/DP_Array_P7_DecodeWays.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class DP_Array_P7_DecodeWays {
 
-    // Approach 1: Top-Down DP (Memoization)
     public int numDecodingsMemo(String s) {
         if (s == null || s.length() == 0) return 0;
         int[] memo = new int[s.length()];
@@ -26,5 +25,27 @@ public class DP_Array_P7_DecodeWays {
             }
         }
         return memo[index] = ways;
+    }
+
+    // Approach 2: Bottom-Up 1D Tabulation
+    public int numDecodingsTabulation(String s) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = Integer.parseInt(s.substring(i - 1, i));
+            int twoDigit = Integer.parseInt(s.substring(i - 2, i));
+
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
     }
 }
