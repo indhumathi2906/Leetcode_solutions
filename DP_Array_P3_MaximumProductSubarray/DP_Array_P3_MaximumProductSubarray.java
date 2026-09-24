3f3f1de4433dc3b8895275541272878abdd82ee7
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:44:58 +0530

    DP Array P3 - Maximum Product Subarray: Add problem description Javadoc

--- DP_Array_P3_MaximumProductSubarray/DP_Array_P3_MaximumProductSubarray.java
+++ DP_Array_P3_MaximumProductSubarray/DP_Array_P3_MaximumProductSubarray.java
@@ -1,5 +1,9 @@
 package DP_Array_P3_MaximumProductSubarray;
 
+/**
+ * DP Array Problem 3: 152. Maximum Product Subarray (LeetCode #152)
+ * Link: https://leetcode.com/problems/maximum-product-subarray/
+ */
 public class DP_Array_P3_MaximumProductSubarray {
 
 }
