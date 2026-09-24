41affb43d725381c0a7776d96a41b9ab90badce9
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:43:57 +0530

    DP Array P2 - House Robber: Add problem description Javadoc

--- DP_Array_P2_HouseRobber/DP_Array_P2_HouseRobber.java
+++ DP_Array_P2_HouseRobber/DP_Array_P2_HouseRobber.java
@@ -1,5 +1,9 @@
 package DP_Array_P2_HouseRobber;
 
+/**
+ * DP Array Problem 2: 198. House Robber (LeetCode #198)
+ * Link: https://leetcode.com/problems/house-robber/
+ */
 public class DP_Array_P2_HouseRobber {
 
 }
