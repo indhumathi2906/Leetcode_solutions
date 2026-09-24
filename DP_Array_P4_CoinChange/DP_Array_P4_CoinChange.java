ed134b37ce9088c0ed95a0a039f89911efdceeb2
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:04 +0530

    DP Array P4 - Coin Change: Add base case validation for zero amount

--- DP_Array_P4_CoinChange/DP_Array_P4_CoinChange.java
+++ DP_Array_P4_CoinChange/DP_Array_P4_CoinChange.java
@@ -1,9 +1,8 @@
 package DP_Array_P4_CoinChange;
 
-/**
- * DP Array Problem 4: 322. Coin Change (LeetCode #322)
- * Link: https://leetcode.com/problems/coin-change/
- */
 public class DP_Array_P4_CoinChange {
 
+    private boolean isZeroAmount(int amount) {
+        return amount == 0;
+    }
 }
