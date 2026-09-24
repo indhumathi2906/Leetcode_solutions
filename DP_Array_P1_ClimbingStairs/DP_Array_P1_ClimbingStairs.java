e7dbf64a136d98885b7e9a50df3e6228a3145dcc
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:43:55 +0530

    DP Array P1 - Climbing Stairs: Implement Bottom-Up DP with Tabulation array

--- DP_Array_P1_ClimbingStairs/DP_Array_P1_ClimbingStairs.java
+++ DP_Array_P1_ClimbingStairs/DP_Array_P1_ClimbingStairs.java
@@ -2,7 +2,6 @@ package DP_Array_P1_ClimbingStairs;
 
 /**
  * DP Array Problem 1: 70. Climbing Stairs (LeetCode #70)
- * Link: https://leetcode.com/problems/climbing-stairs/
  */
 public class DP_Array_P1_ClimbingStairs {
 
@@ -18,5 +17,17 @@ public class DP_Array_P1_ClimbingStairs {
         if (memo[n] != 0) return memo[n];
         memo[n] = memoHelper(n - 1, memo) + memoHelper(n - 2, memo);
         return memo[n];
+    }
+
+    // Approach 2: Bottom-Up DP (Tabulation)
+    public int climbStairsTabulation(int n) {
+        if (n <= 2) return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
