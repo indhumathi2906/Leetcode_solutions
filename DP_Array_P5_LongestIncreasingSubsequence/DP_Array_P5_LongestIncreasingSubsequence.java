06ed65aafeb3fb246b7ba6bf79bf97a850a7a71d
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:11 +0530

    DP Array P5 - LIS: Add edge case checks

--- DP_Array_P5_LongestIncreasingSubsequence/DP_Array_P5_LongestIncreasingSubsequence.java
+++ DP_Array_P5_LongestIncreasingSubsequence/DP_Array_P5_LongestIncreasingSubsequence.java
@@ -1,9 +1,8 @@
 package DP_Array_P5_LongestIncreasingSubsequence;
 
-/**
- * DP Array Problem 5: 300. Longest Increasing Subsequence (LeetCode #300)
- * Link: https://leetcode.com/problems/longest-increasing-subsequence/
- */
 public class DP_Array_P5_LongestIncreasingSubsequence {
 
+    private boolean isEmpty(int[] nums) {
+        return nums == null || nums.length == 0;
+    }
 }
