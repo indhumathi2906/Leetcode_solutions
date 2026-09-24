05e243ea7d9fb42fc0625f7c4c44d4312d90cd35
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:18 +0530

    DP Array P6 - Partition Equal Subset Sum: Implement Top-Down Memoization approach

--- DP_Array_P6_PartitionEqualSubsetSum/DP_Array_P6_PartitionEqualSubsetSum.java
+++ DP_Array_P6_PartitionEqualSubsetSum/DP_Array_P6_PartitionEqualSubsetSum.java
@@ -2,7 +2,26 @@ package DP_Array_P6_PartitionEqualSubsetSum;
 
 public class DP_Array_P6_PartitionEqualSubsetSum {
 
-    private boolean isOddSum(int sum) {
-        return sum % 2 != 0;
+    // Approach 1: Top-Down DP (Memoization)
+    public boolean canPartitionMemo(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
+        if (totalSum % 2 != 0) return false;
+
+        int target = totalSum / 2;
+        Boolean[][] memo = new Boolean[nums.length][target + 1];
+        return memoHelper(nums, 0, target, memo);
+    }
+
+    private boolean memoHelper(int[] nums, int index, int target, Boolean[][] memo) {
+        if (target == 0) return true;
+        if (index >= nums.length || target < 0) return false;
+        if (memo[index][target] != null) return memo[index][target];
+
+        boolean include = memoHelper(nums, index + 1, target - nums[index], memo);
+        boolean exclude = memoHelper(nums, index + 1, target, memo);
+
+        return memo[index][target] = (include || exclude);
     }
 }
