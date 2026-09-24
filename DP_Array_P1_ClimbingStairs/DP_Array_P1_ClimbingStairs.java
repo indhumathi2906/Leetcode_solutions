f4839a4d7c6b3d14cc52e648fd8a75432157c362
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:43:53 +0530

    DP Array P1 - Climbing Stairs: Add problem description Javadoc and complexity target

--- DP_Array_P1_ClimbingStairs/DP_Array_P1_ClimbingStairs.java
+++ DP_Array_P1_ClimbingStairs/DP_Array_P1_ClimbingStairs.java
@@ -1,5 +1,17 @@
 package DP_Array_P1_ClimbingStairs;
 
+/**
+ * DP Array Problem 1: 70. Climbing Stairs (LeetCode #70)
+ * Link: https://leetcode.com/problems/climbing-stairs/
+ *
+ * Description:
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Target Complexities:
+ * Time: O(N)
+ * Space: Top-Down O(N), Bottom-Up O(N), Space-Optimized O(1)
+ */
 public class DP_Array_P1_ClimbingStairs {
 
 }
