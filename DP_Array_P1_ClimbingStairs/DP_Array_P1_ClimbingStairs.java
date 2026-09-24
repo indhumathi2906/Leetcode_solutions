bd3380a36d5637486811bfae2a0ceecf01401b69
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:43:54 +0530

    DP Array P1 - Climbing Stairs: Add base cases and edge condition checks

--- DP_Array_P1_ClimbingStairs/DP_Array_P1_ClimbingStairs.java
+++ DP_Array_P1_ClimbingStairs/DP_Array_P1_ClimbingStairs.java
@@ -7,11 +7,10 @@ package DP_Array_P1_ClimbingStairs;
  * Description:
  * You are climbing a staircase. It takes n steps to reach the top.
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
- *
- * Target Complexities:
- * Time: O(N)
- * Space: Top-Down O(N), Bottom-Up O(N), Space-Optimized O(1)
  */
 public class DP_Array_P1_ClimbingStairs {
 
+    private boolean isValidInput(int n) {
+        return n > 0;
+    }
 }
