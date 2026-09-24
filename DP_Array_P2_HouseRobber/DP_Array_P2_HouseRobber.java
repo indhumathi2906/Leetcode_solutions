9ace847c27fd21a3b1eed649598042ca81fe13fc
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:43:57 +0530

    DP Array P2 - House Robber: Add edge case checks for short arrays

--- DP_Array_P2_HouseRobber/DP_Array_P2_HouseRobber.java
+++ DP_Array_P2_HouseRobber/DP_Array_P2_HouseRobber.java
@@ -2,8 +2,10 @@ package DP_Array_P2_HouseRobber;
 
 /**
  * DP Array Problem 2: 198. House Robber (LeetCode #198)
- * Link: https://leetcode.com/problems/house-robber/
  */
 public class DP_Array_P2_HouseRobber {
 
+    private boolean isEmpty(int[] nums) {
+        return nums == null || nums.length == 0;
+    }
 }
