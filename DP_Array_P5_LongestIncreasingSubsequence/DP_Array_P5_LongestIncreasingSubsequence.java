f2142f41d4f5bc25d9daff46e96edb170a2bf926
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:10 +0530

    DP Array P5 - LIS: Add problem description Javadoc

--- DP_Array_P5_LongestIncreasingSubsequence/DP_Array_P5_LongestIncreasingSubsequence.java
+++ DP_Array_P5_LongestIncreasingSubsequence/DP_Array_P5_LongestIncreasingSubsequence.java
@@ -1,5 +1,9 @@
 package DP_Array_P5_LongestIncreasingSubsequence;
 
+/**
+ * DP Array Problem 5: 300. Longest Increasing Subsequence (LeetCode #300)
+ * Link: https://leetcode.com/problems/longest-increasing-subsequence/
+ */
 public class DP_Array_P5_LongestIncreasingSubsequence {
 
 }
