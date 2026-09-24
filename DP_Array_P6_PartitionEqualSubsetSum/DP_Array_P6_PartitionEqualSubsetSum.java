0d85b5516fd5d475ce1de324987b802ba8d10b73
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:18 +0530

    DP Array P6 - Partition Equal Subset Sum: Add parity check for total sum

--- DP_Array_P6_PartitionEqualSubsetSum/DP_Array_P6_PartitionEqualSubsetSum.java
+++ DP_Array_P6_PartitionEqualSubsetSum/DP_Array_P6_PartitionEqualSubsetSum.java
@@ -1,9 +1,8 @@
 package DP_Array_P6_PartitionEqualSubsetSum;
 
-/**
- * DP Array Problem 6: 416. Partition Equal Subset Sum (LeetCode #416)
- * Link: https://leetcode.com/problems/partition-equal-subset-sum/
- */
 public class DP_Array_P6_PartitionEqualSubsetSum {
 
+    private boolean isOddSum(int sum) {
+        return sum % 2 != 0;
+    }
 }
