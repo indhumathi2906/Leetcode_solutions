445c6b2b54526ac6bf0dd1b75aee95e9c295ca40
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:17 +0530

    DP Array P6 - Partition Equal Subset Sum: Add problem description Javadoc

--- DP_Array_P6_PartitionEqualSubsetSum/DP_Array_P6_PartitionEqualSubsetSum.java
+++ DP_Array_P6_PartitionEqualSubsetSum/DP_Array_P6_PartitionEqualSubsetSum.java
@@ -1,5 +1,9 @@
 package DP_Array_P6_PartitionEqualSubsetSum;
 
+/**
+ * DP Array Problem 6: 416. Partition Equal Subset Sum (LeetCode #416)
+ * Link: https://leetcode.com/problems/partition-equal-subset-sum/
+ */
 public class DP_Array_P6_PartitionEqualSubsetSum {
 
 }
