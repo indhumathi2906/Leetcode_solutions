1bee0e943b6f30fc6e7e31bb84a0d7123d678a17
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:13 +0530

    DP Array P5 - LIS: Implement O(N^2) Bottom-Up DP array

--- DP_Array_P5_LongestIncreasingSubsequence/DP_Array_P5_LongestIncreasingSubsequence.java
+++ DP_Array_P5_LongestIncreasingSubsequence/DP_Array_P5_LongestIncreasingSubsequence.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class DP_Array_P5_LongestIncreasingSubsequence {
 
-    // Approach 1: Top-Down DP (Memoization)
     public int lengthOfLISMemo(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         int[][] memo = new int[nums.length][nums.length + 1];
@@ -23,5 +22,24 @@ public class DP_Array_P5_LongestIncreasingSubsequence {
         int skip = memoHelper(nums, prevIdx, currIdx + 1, memo);
 
         return memo[currIdx][prevIdx + 1] = Math.max(take, skip);
+    }
+
+    // Approach 2: Bottom-Up DP (Tabulation O(N^2))
+    public int lengthOfLISTabulation(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int maxLIS = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLIS = Math.max(maxLIS, dp[i]);
+        }
+        return maxLIS;
     }
 }
