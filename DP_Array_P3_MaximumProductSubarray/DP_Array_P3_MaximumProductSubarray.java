d39a57b4a13d5cd802fd8e1de525193756da7f94
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:44:58 +0530

    DP Array P3 - Maximum Product Subarray: Add edge case validation

--- DP_Array_P3_MaximumProductSubarray/DP_Array_P3_MaximumProductSubarray.java
+++ DP_Array_P3_MaximumProductSubarray/DP_Array_P3_MaximumProductSubarray.java
@@ -1,9 +1,8 @@
 package DP_Array_P3_MaximumProductSubarray;
 
-/**
- * DP Array Problem 3: 152. Maximum Product Subarray (LeetCode #152)
- * Link: https://leetcode.com/problems/maximum-product-subarray/
- */
 public class DP_Array_P3_MaximumProductSubarray {
 
+    private boolean isInvalidInput(int[] nums) {
+        return nums == null || nums.length == 0;
+    }
 }
