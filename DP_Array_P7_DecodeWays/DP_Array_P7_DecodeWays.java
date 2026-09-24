33a5e140688b7e69afae44af467652c843991e1a
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:25 +0530

    DP Array P7 - Decode Ways: Implement Top-Down Memoization recursion

--- DP_Array_P7_DecodeWays/DP_Array_P7_DecodeWays.java
+++ DP_Array_P7_DecodeWays/DP_Array_P7_DecodeWays.java
@@ -1,8 +1,30 @@
 package DP_Array_P7_DecodeWays;
 
+import java.util.Arrays;
+
 public class DP_Array_P7_DecodeWays {
 
-    private boolean hasLeadingZero(String s) {
-        return s == null || s.length() == 0 || s.charAt(0) == '0';
+    // Approach 1: Top-Down DP (Memoization)
+    public int numDecodingsMemo(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int[] memo = new int[s.length()];
+        Arrays.fill(memo, -1);
+        return memoHelper(s, 0, memo);
+    }
+
+    private int memoHelper(String s, int index, int[] memo) {
+        if (index == s.length()) return 1;
+        if (s.charAt(index) == '0') return 0;
+        if (memo[index] != -1) return memo[index];
+
+        int ways = memoHelper(s, index + 1, memo);
+
+        if (index + 1 < s.length()) {
+            int twoDigit = Integer.parseInt(s.substring(index, index + 2));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                ways += memoHelper(s, index + 2, memo);
+            }
+        }
+        return memo[index] = ways;
     }
 }
