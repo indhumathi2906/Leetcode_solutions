9f9264bd8e16520e47273d45691b5171762becfa
Indhumathi <indhumathi.a29@gmail.com>
2026-09-24 21:45:04 +0530

    DP Array P4 - Coin Change: Implement Top-Down Memoization approach

--- DP_Array_P4_CoinChange/DP_Array_P4_CoinChange.java
+++ DP_Array_P4_CoinChange/DP_Array_P4_CoinChange.java
@@ -2,7 +2,26 @@ package DP_Array_P4_CoinChange;
 
 public class DP_Array_P4_CoinChange {
 
-    private boolean isZeroAmount(int amount) {
-        return amount == 0;
+    // Approach 1: Top-Down DP (Memoization)
+    public int coinChangeMemo(int[] coins, int amount) {
+        if (amount < 1) return 0;
+        int[] memo = new int[amount + 1];
+        return memoHelper(coins, amount, memo);
+    }
+
+    private int memoHelper(int[] coins, int rem, int[] memo) {
+        if (rem < 0) return -1;
+        if (rem == 0) return 0;
+        if (memo[rem] != 0) return memo[rem];
+
+        int minCoins = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = memoHelper(coins, rem - coin, memo);
+            if (res >= 0 && res < minCoins) {
+                minCoins = 1 + res;
+            }
+        }
+        memo[rem] = (minCoins == Integer.MAX_VALUE) ? -1 : minCoins;
+        return memo[rem];
     }
 }
